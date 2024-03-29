28e27394468a000f9b6bc4cf8983e30f6f45e639
PyeonRa <Brian3155@naver.com>
2024-03-30 08:53:34 +0900

    토용일...

--- FRONT/src/App.svelte
+++ FRONT/src/App.svelte
@@ -11,10 +11,16 @@
     let storeData = localStorage.getItem('c3RvcmU=')
 
     function connect() {
-        const hostname = window.location.hostname;
-        const serverAddress = hostname === '121.173.41.66'
-            ? 'ws://121.173.41.66:4001/ws'
-            : 'ws://127.0.0.1:4001/ws';
+        const hostname = window.location.hostname
+        console.log(hostname)
+        let serverAddress = ''
+        if (hostname === '172.30.1.12') {
+            serverAddress = 'ws://172.30.1.12:4001/ws'
+        } else if (hostname === 'yullmu.com') {
+            serverAddress = 'ws://yullmu.com/ws'
+        } else {
+            console.log('ws connection error')
+        }
 
         ws = new WebSocket(serverAddress);
 
@@ -116,6 +122,12 @@
         }, 250)
     }
 
+    let audio;
+    
+    function playAudio() {
+        audio.play();
+    }
+
     let imageClicked = false;
 
     function onMouseUp() {
@@ -190,24 +202,31 @@
     let slideNotification = false
 
     function asideSlide(select) {
-        if (slideSetting, select === 'rank') {
+        if (slideSetting === false && slideNotification === false && select === 'rank') {
             slideRank = !slideRank
-        } else if (slideRank, select === 'setting') {
+        } else if (slideRank === false && slideNotification === false && select === 'setting') {
             slideSetting = !slideSetting
-        } else if (slideNotification, select === 'notification') {
+        } else if (slideRank === false && slideSetting === false && select === 'notification') {
             slideNotification = !slideNotification
         }
     }
 
-let update = false
+$: update = true
 </script>
-{#if update}
-<div class="update">
-    <h1>업데이트중!</h1>
-    <p>실시간 업데이트중...</p>
-    <p>(업데이트 일시 중단.)</p>
+<div class="wrap">
+    {#if update}
+    <div class="update">
+        <button on:click={() => update = false}>
+            <span class="material-symbols-outlined">
+                close
+            </span>
+        </button>
+        <h1>공지</h1>
+        <p>사이트가 미완성이므로 오류가 발생할 수 있습니다.</p>
+        <p>문의 : sycd23kr@gmail.com</p>
+    </div>
+    {/if}
 </div>
-{/if}
 
 <main>
     <header>
@@ -229,7 +248,8 @@ let update = false
     </header>
     
     <figure>
-        <button on:mousedown={handleButtonClick} on:mousedown={clickCounter} on:mouseup={onMouseUp} on:mouseleave={onMouseUp}>
+        <audio bind:this={audio} src="/static/고양이효과음.mp3"></audio>
+        <button on:click={playAudio, clickCounter} on:mousedown={handleButtonClick} on:mouseup={onMouseUp} on:mouseleave={onMouseUp}>
             {#if imageClicked}
                 <img id="cat" src="/static/고양이.png" alt="none" draggable="false">
             {:else}
@@ -310,7 +330,12 @@ let update = false
 
     <div class="aside_right_slide" style="right: {slideSetting ? '60px' : '-320px'}">
         <h2 style="color: red;">공사중</h2>
-        <p>공사중</p>
+        <div class="settings">
+            <div class="admin">
+                <input type="text">
+                <button>확인</button>
+            </div>
+        </div>
     </div>     
 </div>
 
@@ -336,25 +361,75 @@ let update = false
 {/if}
 
 <style>
+    .wrap {
+        height: 100%;
+        width: 100%;
+
+        top: 0;
+        left: 0;
+        position: absolute;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        overflow-x: hidden;
+
+        pointer-events: none;
+        user-select: none;
+    }
+
     .update {
         height: 200px;
         width: 300px;
 
-        top: 0;
-        left: 15vw;
         position: absolute;
+        z-index: 10;
 
         display: flex;
         flex-direction: column;
         align-items: center;
 
-        background-color: #8888889a;
+        background-color: rgb(210, 210, 210);
         border: 2px solid black;
         border-radius: 10px;
+
+        pointer-events: auto;
+        user-select: auto;
     }
 
     .update h1{
+        margin: 10px;
+
         color: red;
+    }
+
+    .update p {
+        width: 80%;
+
+        margin: 5px;
+    }
+
+    .update button {
+        height: 30px;
+        width: 30px;
+
+        margin: 0;
+        padding: 0;
+
+        top: 10px;
+        right: 10px;
+        position: absolute;
+        z-index: 15;
+        
+        background: none;
+        border: none;
+
+        cursor: pointer;
+    }
+
+    .update button span {
+        font-size: 30px;
     }
 
     main {
@@ -578,6 +653,7 @@ let update = false
         writing-mode: vertical-rl;
 
         background: none;
+        background-color: #d3d3d3;
         border-left: none;
         border-top: 3px solid black;
         border-right: 3px solid black;
@@ -637,7 +713,11 @@ let update = false
         height: 76vh;
         width: 300px;
 
+        padding: 0;
+        margin: 0;
+
         position: absolute;
+        z-index: 20;
 
         background-color: rgb(210, 210, 210);
         border-top: 3px solid black;
@@ -668,6 +748,14 @@ let update = false
     .aside_right_slide h2 {
         font-size: 18px;
         text-align: center;
+    }
+
+    .aside_right_slide .settings {
+        width: 50%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 
     sub {
